425b0805f17b9a9d10c734bbd51b1a24b3d84756
Chris Okorodudu <christopherokorodudu@172-16-61-250.DYNAPOOL.NYU.EDU>
2018-04-12 12:20:06 -0400

    testing

--- db.js
+++ db.js
@@ -1,45 +1,45 @@
 
 
-const mongoose = require('mongoose');
-const URLSlugs = require('mongoose-url-slugs');
+// const mongoose = require('mongoose');
+// const URLSlugs = require('mongoose-url-slugs');
 
 
 
 
 
-const CommentSchema = new mongoose.Schema({
-	userID: {type: String, required: true}, 
-	postID: {type: String, required: true},
-	comment: String, 
-	createdAT: String
-});
+// const CommentSchema = new mongoose.Schema({
+// 	userID: {type: String, required: true}, 
+// 	postID: {type: String, required: true},
+// 	comment: String, 
+// 	createdAT: String
+// });
 
 
-const PostSchema = new mongoose.Schema({
-	userID: {type: String, required: true}, //must be logged in to post or comment
-	title: {type: String, required: true},
-	url: {type: String}, //optional url to outside website 
-	status: {type: String, required: true},
-	comments: [CommentSchema],
-	createAt: String, //timestamp
-});
+// const PostSchema = new mongoose.Schema({
+// 	userID: {type: String, required: true}, //must be logged in to post or comment
+// 	title: {type: String, required: true},
+// 	url: {type: String}, //optional url to outside website 
+// 	status: {type: String, required: true},
+// 	comments: [CommentSchema],
+// 	createAt: String, //timestamp
+// });
 
-const UserSchema = new mongoose.Schema ({
-    username: {type: String, unique: true, required: true},
-    email: {type: String, unique: true, required: true}, 
-    password: {type: String, unique: true, required: true},
-    posts: [PostSchema],
-    comments: [CommentSchema]
-});
-
-
-PostSchema.plugin(URLSlugs('username title'));
+// const UserSchema = new mongoose.Schema ({
+//     username: {type: String, unique: true, required: true},
+//     email: {type: String, unique: true, required: true}, 
+//     password: {type: String, unique: true, required: true},
+//     posts: [PostSchema],
+//     comments: [CommentSchema]
+// });
 
 
-// "registring" your schema
-// name as a string mapped to actual schema object
-mongoose.model('Comment', CommentSchema);
-mongoose.model('Post', PostSchema);
-mongoose.model('User', UserSchema);
+// PostSchema.plugin(URLSlugs('username title'));
 
-mongoose.connect('mongodb://localhost/27017/basketball');
+
+// // "registring" your schema
+// // name as a string mapped to actual schema object
+// mongoose.model('Comment', CommentSchema);
+// mongoose.model('Post', PostSchema);
+// mongoose.model('User', UserSchema);
+
+// mongoose.connect('mongodb://localhost/27017/basketball');
